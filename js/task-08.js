a8b333eeb2b995e1a4db7ee6c2a38d899f40109a
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-08-21 02:33:26 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value.length === 0 || password.value.length === 0) {
+        console.warn('Всі поля повинні бути заповнені!');
+    }
+    else {
+        const answear = {email: `${email.value}`, password: `${password.value}`}
+        console.log(answear);
+        form.reset();
+    }
+});
