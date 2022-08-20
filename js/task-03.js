10e1c7d5aadce6797896d370b09411ebecf9e586
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-08-20 22:47:24 +0300

    task 3 done

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery');
+
+const imgTags = images.map((image) => `<li class="item"><img class="img" href="${image.url}", alt="${image.alt}"></img></li>`).join("");
+
+list.insertAdjacentHTML("afterbegin", imgTags);
