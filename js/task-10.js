6bb84db0902bc3b490abe3c8ddbaa3081dbe673d
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-08-21 22:44:15 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+// const button = document.querySelector('#controls > button');
+
+const buttonCreate = document.querySelector('[data-create]');
+// const buttonDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+let amount = 2
+
+const numberOfBoxes = () => {
+  return input.value;
+  };
+
+// amount = numberOfBoxes();
+
+// button.addEventListener('click', amount);
+
+
+
+buttonCreate.addEventListener('click', numberOfBoxes);
+
+// amount = numberOfBoxes();
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divBox = document.createElement("div");
+    divBox.style.width = "10px";
+    divBox.style.heigth = "10px";
+    divBox.style.backgroundColor = `${getRandomHexColor()}`;
+    boxes.append(divBox);
+}
+};
+
+createBoxes(numberOfBoxes());
+
+
