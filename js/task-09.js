693913292ee82eca11efce66cfb338f28bc63691
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-08-21 17:50:28 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorName = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+const changeColor = () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorName.textContent = `${getRandomHexColor()}`;
+};
+
+btn.addEventListener('click', changeColor);
